e3f120396dbc76612acc9846380e0cc531fc53f8
ancientcheeze <ancientcheeze@gmail.com>
2025-11-20 01:00:45 +0200

    update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const envelope = document.querySelector(".envelope-wrapper");
 
+
+
+        const heart = document.querySelector(".heart");
+
+
+        heart.addEventListener("click", ()=>{
+
+
+            envelope.classList.toggle("flap")
+
+
+        })
